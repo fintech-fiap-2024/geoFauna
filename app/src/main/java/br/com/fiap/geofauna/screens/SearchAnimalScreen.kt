edd9abafb3e97dfcfaeabf91ec3668759312c87e
Geovani carlos de souza <94084031+Geovani237@users.noreply.github.com>
2025-03-14 23:31:35 -0300

    adicionando retrofit

--- app/src/main/java/br/com/fiap/geofauna/screens/SearchAnimalScreen.kt
+++ app/src/main/java/br/com/fiap/geofauna/screens/SearchAnimalScreen.kt
@@ -1,37 +1,43 @@
 package br.com.fiap.geofauna.screens
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
-import br.com.fiap.geofauna.R
-import br.com.fiap.geofauna.ui.theme.Exo
-import br.com.fiap.geofauna.ui.theme.SourceSerif
-
+import br.com.fiap.geofauna.components.CaixaDeEntrada
 
 @Composable
 fun SearchAnimalScreen(navController: NavController) {
-    Column() {
-        Text(
-            text = "LOGIN",
-            modifier = Modifier
-                .fillMaxWidth(),
-            fontSize = 32.sp,
-            fontFamily = SourceSerif,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            color = colorResource(id = R.color.gray)
+
+    var searchAnimal by remember {
+        mutableStateOf("")
+    }
+
+    Box(
+        modifier = Modifier
+            .background(Color.Gray)
+            .fillMaxSize()
+    ){
+        CaixaDeEntrada(
+            value = searchAnimal,
+            onValueChange = { searchAnimal = it },
+            label = "Nome do Animal",
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "")
+                }
+            }
         )
     }
 }
